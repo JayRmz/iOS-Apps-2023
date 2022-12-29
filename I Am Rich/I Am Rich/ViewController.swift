37aa35d8bc80f1f82d9f5c51f6732489f37de269
Juan Ramírez Blancas <jayrmzdev@Juans-iMac.local>
2022-12-28 23:32:45 -0600

    Iam rich app done

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Juan Ramírez Blancas on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

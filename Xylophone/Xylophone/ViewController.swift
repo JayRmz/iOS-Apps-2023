d26f0364f48af152bcba843ed0508ab91c387cad
Juan Manuel <juanmanuel@MacBook-Air-de-Sistemas.local>
2023-01-04 12:14:30 -0600

    Xylophone done

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("I got pressed: ", sender.tag)
+        let sound = sender.currentTitle ?? "C"
+        playSound(sound: sound)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(sound: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
